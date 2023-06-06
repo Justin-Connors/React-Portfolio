3bd7f62bdbaad0a11c0ba2fe6663d9868cb94fb2
justin connors <justinconnors@hotmail.ca>
2023-06-06 16:26:50 -0300

    added linkedIn and GitHub button links

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -13,11 +13,11 @@ function Footer() {
     <MDBFooter className='bg-dark text-center text-white fixed-bottom'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/justin-connors-0a3b42127/' role='button' target="_blank">
             <MDBIcon fab icon='linkedin-in' />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://github.com/Justin-Connors' role='button' target="_blank">
             <MDBIcon fab icon='github' />
           </MDBBtn>
         </section>
