036ec62be490b5de80a9927d010226029e43ffcb
justin connors <justinconnors@hotmail.ca>
2023-06-07 18:54:41 -0300

    removed mailer stuff, going to do it differently later on

--- my-portfolio/src/components/Footer.js
+++ my-portfolio/src/components/Footer.js
@@ -10,25 +10,27 @@ import {
 
 function Footer() {
   return (
-    <MDBFooter className='bg-dark text-center text-white fixed-bottom'>
-      <MDBContainer className='p-4 pb-0'>
-        <section className='mb-4'>
-          <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/justin-connors-0a3b42127/' role='button' target="_blank">
-            <MDBIcon fab icon='linkedin-in' />
-          </MDBBtn>
+    <div className='fixed-bottom'>
+      <MDBFooter className='bg-dark text-center text-white'>
+        <MDBContainer className='p-4 pb-0'>
+          <section className='mb-4'>
+            <MDBBtn outline color="light" floating className='m-1' href='https://www.linkedin.com/in/justin-connors-0a3b42127/' role='button' target="_blank">
+              <MDBIcon fab icon='linkedin-in' />
+            </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='https://github.com/Justin-Connors' role='button' target="_blank">
-            <MDBIcon fab icon='github' />
-          </MDBBtn>
-        </section>
-      </MDBContainer>
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        © 2023 Copyright: &nbsp;
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          Justin Connors
-        </a>
-      </div>
-    </MDBFooter>
+            <MDBBtn outline color="light" floating className='m-1' href='https://github.com/Justin-Connors' role='button' target="_blank">
+              <MDBIcon fab icon='github' />
+            </MDBBtn>
+          </section>
+        </MDBContainer>
+        <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+          © 2023 Copyright: &nbsp;
+          <a className='text-white' href='https://mdbootstrap.com/'>
+            Justin Connors
+          </a>
+        </div>
+      </MDBFooter>
+    </div>
   );
 }
 
--- my-portfolio/src/components/NavBar.js
+++ my-portfolio/src/components/NavBar.js
@@ -15,51 +15,53 @@ function NavBar({ currPage, handlePageChange }) {
   const [showNav, setShowNav] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' dark bgColor='dark' fixed='top'>
-      <MDBContainer fluid>
-        <MDBNavbarBrand
-        href='#home'
-        onClick={() => handlePageChange('Home')}
-            className={currPage === 'Home' ? 'nav-link active' : 'nav-link'}
-            >Justin Connors
-        </MDBNavbarBrand>
-        <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
-          onClick={() => setShowNav(!showNav)}
-        >
-          <MDBIcon icon='bars' fas />
-        </MDBNavbarToggler>
-        <MDBCollapse navbar show={showNav}>
-          <MDBNavbarNav>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#about'
-              onClick={() => handlePageChange('About')}
-              className={currPage === 'About' ? 'nav-link active' : 'nav-link'}
-              >About
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#projects'
-              onClick={() => handlePageChange('Projects')}
-              className={currPage === 'Projects' ? 'nav-link active' : 'nav-link'}
-              >Projects
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-              <MDBNavbarLink href='#contact'
-              onClick={() => handlePageChange('Contact')}
-              className={currPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-              >Contact
-              </MDBNavbarLink>
-            </MDBNavbarItem>
-            <MDBNavbarItem>
-            </MDBNavbarItem>
-          </MDBNavbarNav>
-        </MDBCollapse>
-      </MDBContainer>
-    </MDBNavbar>
+    <div className='fixed-top'>
+      <MDBNavbar expand='lg' dark bgColor='dark'>
+        <MDBContainer fluid>
+          <MDBNavbarBrand
+          href='#home'
+          onClick={() => handlePageChange('Home')}
+              className={currPage === 'Home' ? 'nav-link active' : 'nav-link'}
+              >Justin Connors
+          </MDBNavbarBrand>
+          <MDBNavbarToggler
+            type='button'
+            aria-expanded='false'
+            aria-label='Toggle navigation'
+            onClick={() => setShowNav(!showNav)}
+          >
+            <MDBIcon icon='bars' fas />
+          </MDBNavbarToggler>
+          <MDBCollapse navbar show={showNav}>
+            <MDBNavbarNav>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#about'
+                onClick={() => handlePageChange('About')}
+                className={currPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >About
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#projects'
+                onClick={() => handlePageChange('Projects')}
+                className={currPage === 'Projects' ? 'nav-link active' : 'nav-link'}
+                >Projects
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+                <MDBNavbarLink href='#contact'
+                onClick={() => handlePageChange('Contact')}
+                className={currPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >Contact
+                </MDBNavbarLink>
+              </MDBNavbarItem>
+              <MDBNavbarItem>
+              </MDBNavbarItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
+        </MDBContainer>
+      </MDBNavbar>
+    </div>
   );
 }
 
